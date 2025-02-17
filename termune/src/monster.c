55d508bfe8326b37b9d1086ec32fc3dd57ae0017
Connor Tynan <connortynan16@gmail.com>
2025-02-17 14:04:22 -0600

    Added monster.h declarations and implemented monster_print_distance_map() (Untested)

---
+++ termune/src/monster.c
@@ -0,0 +1,22 @@
+#include "monster.h"
+
+int monster_print_distance_map(
+    monster_distance_map *dist_map,
+    dungeon_data *dungeon)
+{
+    uint8_t x, y;
+    for (y = 0; y < DUNGEON_HEIGHT; y++)
+    {
+        for (x = 0; x < DUNGEON_WIDTH; x++)
+        {
+            if (x == dungeon->pc_x && y == dungeon->pc_y)
+                printf("@");
+            else if (dist_map->distances[x][y] == UINT32_MAX)
+                printf(" ");
+            else
+                printf("%1d", dist_map->distances[x][y] % 10);
+        }
+        printf("\n");
+    }
+    return 0;
+}
